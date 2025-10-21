bda4b57594f7347ce915cf8f5dd2df46ddd8de0d
zakiyyah258777-hub <zakiyyah258777@student.smkn1kandeman.sch.id>
2025-10-21 09:28:23 +0700

    Add files via upload

---
+++ praktik4.1_zakiyyah_x pplg2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contoh_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Memeriksa apakah seseorang sudah remaja
+            Console.WriteLine("Masukkan Usia anda : ");
+            int usia = int.Parse(Console.ReadLine());
+            if (usia >= 13)
+            {
+                Console.WriteLine("Anda Sudah memasuki masa remaja.");
+            }
+        }
+    }
+}
